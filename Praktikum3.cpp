537de9045a13653833b00153a23891c44de0166c
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-03-06 15:27:45 +0700

    void nya

--- Praktikum3.cpp
+++ Praktikum3.cpp
@@ -3,3 +3,32 @@ using namespace std;
 
 int a[20];
 int n;
+
+void input()
+{
+    while(true)
+    {
+        cout << "Masukkan panjang array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "error : Maksimal input array adalah 20\n";
+        }
+    } 
+
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < n; i++) //i++ / i = i + 1
+    {
+        cout << "Masukkan Nilai ke -" << i + 1 << " : ";
+        cin >> a[i];
+    }
+}
